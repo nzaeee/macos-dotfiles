a615ecf507a2b3680be5cb64dc29478e3d970b69
nzxcx <hugo.laclau@gmail.com>
2025-01-05 23:51:40 +0100

    feat(nvim): add mini plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
+require("config.lazy")
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
 
-require("config.lazy")
+vim.opt.shiftwidth = 4
