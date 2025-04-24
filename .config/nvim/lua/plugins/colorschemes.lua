1a05723ebec63aa0f0994906d27e76705de828b1
nzxcx <hugo.laclau@gmail.com>
2025-04-25 00:03:16 +0200

    feat: catppuccin theme for neovim

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,16 +1,42 @@
 return {
   {
-    "bluz71/vim-moonfly-colors",
-    name = "moonfly",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     config = function()
-      -- leave this setup function empty for default config
-      -- or refer to the configuration section
-      -- for configuration options
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        transparent_background = false,
+        term_colors = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          telescope = true,
+          mason = true,
+          treesitter = true,
+          which_key = true,
+          indent_blankline = {
+            enabled = true,
+            colored_indent_levels = false,
+          },
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+            },
+          },
+        },
+      })
+      vim.cmd.colorscheme "catppuccin"
     end,
-    init = function()
-      vim.cmd("colorscheme moonfly")
-    end
   }
 }
